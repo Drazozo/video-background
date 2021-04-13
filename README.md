dfc06875245f9c2dfadac36bcf0ed07e78460bfe
Drazozo <mr.metayer.enzo@gmail.com>
2021-04-13 14:48:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# video-background
+ Ce repo m'a permis en un instant de mettre sur le site d'un de mes clients une vidéo en background. Ce n'est pas la même vidéo, celle-ci simplement pour montrer que ça fonctionne et est assez légère. 
---
+++ index.php
@@ -0,0 +1,14 @@
+<video poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/polina.jpg" id="bgvid" playsinline autoplay muted loop>
+  <!-- 
+- Video needs to be muted, since Chrome 66+ will not autoplay video with sound.
+WCAG general accessibility recommendation is that media such as background video play through only once. Loop turned on for the purposes of illustration; if removed, the end of the video will fade in the same way created by pressing the "Pause" button  -->
+<source src="http://thenewcode.com/assets/videos/polina.webm" type="video/webm">
+<source src="alathai-v2.mp4" type="video/mp4">
+
+</video>
+<div id="polina">
+<h1>A la thai</h1>
+
+Utiliser pour mettre sur le site de A la thai en background. 
+<button>Pause</button>
+</div>
